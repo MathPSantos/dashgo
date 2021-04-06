9ce19e26ed4d9094ed906405df5990116ac30922
MathPSantos <matheus.psantos2016@gmail.com>
2021-04-06 12:11:10 -0300

    feat: divide components in smaller components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+
+export function Profile() {
+  return(
+    <Flex
+        align="center"
+      >
+        <Box mr="4" textAlign="right">
+          <Text>Matheus Santos</Text>
+          <Text color="gray.300" fontSize="small">
+            matheus.psantos2016@gmail.com
+          </Text>
+        </Box>
+
+        <Avatar size="md" name="Matheus Santos" src="https://github.com/MathPSantos.png" />
+      </Flex>
+  )
+}
